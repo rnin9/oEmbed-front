3f4b6160bfc4a30c2d476f07a76729fb7587e7c4
=rnin9 <=rkdalswn1209>
2022-01-15 23:26:49 +0900

    not working on useState value

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Input, Divider } from 'antd';
 const { Search } = Input;
 
 function App() {
+  const [data, setData] = useState({});
+  const [url, setUrl] = useState('')
+  useEffect(() => {
+    onSearch()
+  }, [data])
 
-  const onSearch = (value) => {
-    console.log(value)
-    axios.get(`http://localhost:3001/data?url=${value}`).then((res) => { console.log(res) }).catch((err) => {
-      console.log(err)
-    })
-
+  const onSearch = () => {
+    const response = axios.get(`http://localhost:3001/data?url=${url}`)
+    setData(response.data)
+    console.log(data)
   }
   return (
     <div className='outerFrame'>
       <header className='headContent'>
         <h1>oEmbed Test</h1>
-        <Search placeholder="Input URL Scheme text" onSearch={onSearch} enterButton="확인" color='#f1f3f4' />
+        <input color='#f1f3f4' name="value" type="text" onChange={(e) => { setUrl(e.target.value) }} />
+        <input type="button" onClick={onSearch()} />
       </header >
-      <content>
-      
-      </content>
+      <div>
+        <ul>
+          {data && Object.entries(data).forEach((keys, val) => {
+            <li key={val}>{keys}: {val}</li>
+          })}
+        </ul>
+      </div>
       <footer>
         <Divider />
       </footer>
