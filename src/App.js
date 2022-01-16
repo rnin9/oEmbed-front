6a13b3d73344481a677cd9b9d559145114422e02
=rnin9 <=rkdalswn1209>
2022-01-16 13:12:52 +0900

    display data with ul,li tag & add comment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Input, Divider, Descriptions, } from 'antd';
+import { Input, Divider } from 'antd';
 const { Search } = Input;
 
 function App() {
+  //use hooks
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(false)
+
+  //when Search Button is clicked
   const onSearch = (value) => {
     axios.get(`http://localhost:3001/data?url=${value}`).then((res) => {
       setData(res.data)
       setIsLoading(true)
-
     }).catch((err) => {
       console.log(err)
     })
@@ -20,24 +22,27 @@ function App() {
     <div className='outerFrame'>
       <div className='headContent'>
         <h1>oEmbed Test</h1>
+        {/* Search field, using Ant Design UI Framework */}
         <Search placeholder="input search text" onSearch={onSearch} enterButton="확인" />
       </div >
-      <div>
-        <Descriptions>{
+      {/* content field, loop oEmbed Data Object */}
+      <div className='content'>
+        { isLoading &&
           Object.entries(data).map(([key, value]) => {
-            return(<Descriptions.Item label={key} span={3}>
-              {value}
-            </Descriptions.Item>)
+            return (
+              <ul>
+                <li className='liKey'>{key}</li>
+                <li className='liValue'>{value}</li>
+              </ul>
+            )
           })
         }
-        </Descriptions>
       </div>
       <footer>
         <Divider />
       </footer>
     </div >
   );
-
 }
 
 
