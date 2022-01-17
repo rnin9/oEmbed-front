af2b5f3ccd0bda2b3f2bd852cee820a4c237fd53
=rnin9 <=rkdalswn1209>
2022-01-17 17:47:22 +0900

    add message(check statusCode) and get datas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Input, Divider } from 'antd';
+import { Input, Divider, message } from 'antd';
 const { Search } = Input;
 
 function App() {
-  //use hooks
+  //use react hooks
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(false)
 
-  //when Search Button is clicked
-  const onSearch = (value) => {
-    axios.get(`http://localhost:3001/data?url=${value}`).then((res) => {
-      const sortedData = sortData(res.data);
-      setData(sortedData)
-      setIsLoading(true)
-    }).catch((err) => {
-      console.log(err)
-    })
+  const onSearch = async (value) => {
+    try {
+      const response = await axios.get('http://localhost:3001/data', { params: { url: value } })
+      const { data, status } = response
+      if (status === 200) {
+        const sortedData = sortData(data)
+        setData(sortedData)
+        setIsLoading(true)
+        message.success('Success to get oEmbed Data!')
+      }
+      else {
+        message.error(`Error : unknown DESC: please let developers to know this Problem`)
+      }
+    }
+    catch (err) {
+      setData({})
+      const { data, status } = err.response
+      message.error(`Error: ${status} DESC: ${data.message}`)
+    }
   }
-// 위에거 발전방향 => 400 오류에따라서 alert오는거 다르게, 화면 바뀌게, 
+  // 위에거 발전방향 => 400 오류에따라서 alert오는거 다르게, 화면 바뀌게, 
   const sortData = (data) => {
     if (data.hasOwnProperty('title')) {
       const { title, ...restData } = data
@@ -33,7 +43,7 @@ function App() {
       <div className='headContent'>
         <h1>oEmbed Test</h1>
         {/* Search field, using Ant Design UI Framework */}
-        <Search placeholder="input search text" onSearch={onSearch} enterButton="확인" />
+        <Search placeholder="input search URL Scheme" onSearch={onSearch} enterButton="확인" />
       </div >
       {/* content field, loop oEmbed Data Object */}
       <div className='content'>
