a0668e1649483c71adc4254b02e908d051a227cc
=rnin9 <=rkdalswn1209>
2022-01-17 19:51:49 +0900

    preview html&thumbnail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React, { useState } from 'react';
+import Parser from 'html-react-parser';
 import axios from 'axios';
-import { Input, Divider, message } from 'antd';
+import { Input, Divider, message, Image } from 'antd';
 const { Search } = Input;
 
 function App() {
@@ -9,10 +10,17 @@ function App() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(false)
 
+  /* @brief main Logic when search button isClicked, get oEmbed data from custom backend server.
+   * @date 22/01/16
+   * @return none  : set state value and print message to client
+   * @param value(string) : url Scheme which client typing input
+  */
   const onSearch = async (value) => {
     try {
+      //get response from custom Backend Server
       const response = await axios.get('http://localhost:3001/data', { params: { url: value } })
       const { data, status } = response
+      //when data come from server with 200 statusCode, Sorting Data and show view to client
       if (status === 200) {
         const sortedData = sortData(data)
         setData(sortedData)
@@ -20,16 +28,23 @@ function App() {
         message.success('Success to get oEmbed Data!')
       }
       else {
+        //when unknown error occurred
         message.error(`Error : unknown DESC: please let developers to know this Problem`)
       }
     }
     catch (err) {
+      //when backend Server throw the error, reset view and show message to client
       setData({})
       const { data, status } = err.response
       message.error(`Error: ${status} DESC: ${data.message}`)
     }
   }
-  // 위에거 발전방향 => 400 오류에따라서 alert오는거 다르게, 화면 바뀌게, 
+
+  /* @brief sort response data Logic, especially make title key of response object to top
+   * @date 22/01/16
+   * @return sorted Data (object)  : object which of title key is top
+   * @param unsorted Data (object) : response data from backend Server
+  */
   const sortData = (data) => {
     if (data.hasOwnProperty('title')) {
       const { title, ...restData } = data
@@ -38,6 +53,7 @@ function App() {
     return data;
   }
 
+  //view code
   return (
     <div className='outerFrame'>
       <div className='headContent'>
@@ -49,12 +65,38 @@ function App() {
       <div className='content'>
         {isLoading &&
           Object.entries(data).map(([key, value]) => {
-            return (
-              <ul key={key}>
+            if (key === 'title') {
+              return (
+                <ul key={key}>
+                  <li className='liKey'>{key}</li>
+                  <li className='liValue'><h3>{value}</h3></li>
+                </ul>
+              )
+            } else if (key === 'thumbnail_url') {
+              // TODO: flex changing
+              return (<ul key={key}>
                 <li className='liKey'>{key}</li>
-                <li className='liValue'>{value}</li>
+                <li className='liValue'><a href={value}>{value}</a></li>
+                <li className='liValue'><Image src={value} width={data.thumbnail_width.toString()} height={data.thumbnail_height.toString()} alt="asd">{value}</Image></li>
               </ul>
-            )
+              )
+            } else if (key === 'html') {
+              return (<ul key={key}>
+                <li className='liKey'>{key}</li>
+                <div>
+                  {Parser(value)}
+                </div>
+              </ul>
+              )
+            }
+            else {
+              return (
+                <ul key={key}>
+                  <li className='liKey'>{key}</li>
+                  <li className='liValue'>{value}</li>
+                </ul>
+              )
+            }
           })
         }
       </div>
